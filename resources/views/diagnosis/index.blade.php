e90e6bd3880fded8c67245baf0d548e9d1ff09d0
faiz832 <faqodefazz@gmail.com>
2024-12-07 18:33:38 +0700

    fix

--- resources/views/diagnosis/index.blade.php
+++ resources/views/diagnosis/index.blade.php
@@ -6,27 +6,28 @@
     @endif
 
     <div class="rounded-md p-6 border border-gray-200 overflow-auto">
-        <h1 class="text-lg font-medium text-gray-900 mb-4">Riwayat Analisis Kamu</h1>
-        <table class="w-full table-auto rounded-lg overflow-hidden">
+        <h1 class="text-lg font-medium text-gray-900">Diagnosis History</h1>
+        <p class="mt-1 text-sm text-gray-600">Here is your diagnosis history</p>
+        <table class="mt-6 w-full table-auto rounded-lg overflow-hidden">
             <thead class="border-b border-gray-200">
                 <tr>
                     <th class="px-4 py-3 text-center text-sm font-medium text-gray-900 tracking-wider w-12">
                         No
                     </th>
                     <th class="px-4 py-3 text-center text-sm font-medium text-gray-900 tracking-wider">
-                        Gangguan
+                        Disorder
                     </th>
                     <th class="px-4 py-3 text-center text-sm font-medium text-gray-900 tracking-wider">
-                        Kepastian
+                        Certainity
                     </th>
                     <th class="px-4 py-3 text-center text-sm font-medium text-gray-900 tracking-wider">
-                        Rekomendasi
+                        Recommendation
                     </th>
                     <th class="px-4 py-3 text-center text-sm font-medium text-gray-900 tracking-wider">
-                        Tanggal
+                        Date
                     </th>
                     <th class="px-4 py-3 text-center text-sm font-medium text-gray-900 tracking-wider">
-                        Terapi
+                        Therapy
                     </th>
                 </tr>
             </thead>
