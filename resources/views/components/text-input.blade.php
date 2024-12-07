cc2a86d285396d9764ba2451b5db0aa812384f8c
faiz832 <faqodefazz@gmail.com>
2024-12-07 16:56:00 +0700

    fix button

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,4 +1,4 @@
 @props(['disabled' => false])
 
 <input @disabled($disabled)
-    {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-full shadow-sm']) }}>
+    {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
