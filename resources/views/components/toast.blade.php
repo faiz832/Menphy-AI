5b9c0dcf6e843352e4eafc7f2afe04f6295a6f0e
faiz832 <faqodefazz@gmail.com>
2024-12-07 19:31:24 +0700

    fix toast

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -15,8 +15,8 @@
     x-transition:enter-end="translate-x-0" x-transition:leave="transform transition ease-in duration-300"
     x-transition:leave-start="translate-x-0" x-transition:leave-end="translate-x-full" class="fixed top-4 right-4 z-50">
     <div class="{{ $classes }} text-white px-6 py-4 rounded-md shadow-lg">
-        <div class="flex items-center">
-            <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+        <div class="flex items-center max-w-xs gap-4">
+            <svg class="shrink-0 w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
