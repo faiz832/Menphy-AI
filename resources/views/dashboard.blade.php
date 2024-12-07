e02978563c7c6aac9adb426db83935c48a16a214
faiz832 <faqodefazz@gmail.com>
2024-12-07 17:31:46 +0700

    fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -26,27 +26,28 @@
     </div>
 
     <div class="rounded-md p-6 border border-gray-200 overflow-auto">
-        <h1 class="text-lg font-medium text-gray-900 mb-4">Riwayat Analisis Kamu</h1>
-        <table class="w-full table-auto rounded-lg overflow-hidden">
+        <h1 class="text-lg font-medium text-gray-900">Diagnosis History</h1>
+        <p class="mt-1 text-sm text-gray-600">Here is your diagnosis history</p>
+        <table class="mt-6 w-full table-auto rounded-lg overflow-hidden">
             <thead class="border-b border-gray-200">
                 <tr>
                     <th class="px-4 py-3 text-center text-sm font-medium text-gray-900 tracking-wider w-12">
                         No
                     </th>
                     <th class="px-4 py-3 text-center text-sm font-medium text-gray-900 tracking-wider">
-                        Gangguan
+                        Disorder
                     </th>
                     <th class="px-4 py-3 text-center text-sm font-medium text-gray-900 tracking-wider">
-                        Kepastian
+                        Certainainty
                     </th>
                     <th class="px-4 py-3 text-center text-sm font-medium text-gray-900 tracking-wider">
-                        Rekomendasi
+                        Recommendation
                     </th>
                     <th class="px-4 py-3 text-center text-sm font-medium text-gray-900 tracking-wider">
-                        Tanggal
+                        Date
                     </th>
                     <th class="px-4 py-3 text-center text-sm font-medium text-gray-900 tracking-wider">
-                        Lihat
+                        Action
                     </th>
                 </tr>
             </thead>
@@ -76,13 +77,9 @@
                             {{ $diagnosis->created_at->format('d M Y') }}
                         </td>
                         <td class="px-4 py-4 text-center whitespace-nowrap text-sm text-gray-900">
-                            <a href="{{ url('/diagnosis/result/' . $diagnosis->id) }}" class="flex justify-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 24 24"
-                                    fill="currentColor">
-                                    <path
-                                        d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z">
-                                    </path>
-                                </svg>
+                            <a href="{{ url('/diagnosis/result/' . $diagnosis->id) }}"
+                                class="px-2.5 py-1.5 text-xs text-white rounded-md bg-gray-900 hover:bg-gray-700">
+                                view
                             </a>
                         </td>
                     </tr>
